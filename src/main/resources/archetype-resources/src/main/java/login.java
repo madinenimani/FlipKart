80677ae4a9189202a438e9058aee4e5ff0a6fe73
madinenimani <manikantamadieni79@gmail.com>
2024-12-03 15:17:41 +0000

    login

---
+++ src/main/resources/archetype-resources/src/main/java/login.java
@@ -0,0 +1,13 @@
+package $package;
+
+/**
+ * Hello world!
+ *
+ */
+public class Login
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is the Login Page:!" );
+    }
+}
